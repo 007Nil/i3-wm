df97151a43b880dc80868a1b99193c69a4a8b7c5
007Nil <sagniksarkar@ymail.com>
2021-11-21 06:25:26 +0000

    added dynamic_monitor_name_fetch function

--- i3/bin/external_monitor.py
+++ i3/bin/external_monitor.py
@@ -7,7 +7,7 @@ import subprocess
 INTERNAL = "eDP-1-1"
 #EXTERNAL = "HDMI1"
 EXTERNAL = "HDMI-1-1"
-HDMI_CONNECT = "HDMI-1-1 connected"
+HDMI_CONNECT = f"{EXTERNAL} connected"
 #PRIMARY_CMD = "xrandr --output HDMI-1-1 --right-of LVDS1"
 
 
@@ -16,11 +16,13 @@ def is_connected():
     if HDMI_CONNECT in xrandr_output:
         return True
     else:
+        print("HIT")
         return False
 
 
 def exe_cmd(direction):
-    active_internal_cmd = 'xrandr --output eDP-1-1 --primary --mode 1366x768'
+    active_internal_cmd = f'xrandr --output {INTERNAL} --primary --mode 1366x768'
+    print(active_internal_cmd)
     PRIMARY_CMD = "xrandr --output {} --{}-of {} --mode 1920x1080".format(
         EXTERNAL, direction, INTERNAL)
     feh_cmd = "feh --bg-scale /home/$USER/Downloads/anime.jpg"
@@ -39,16 +41,42 @@ def right():
         exe_cmd("right")
 
 def external():
-    external_cmd = 'xrandr --output eDP-1-1 --off'
+    external_cmd = f'xrandr --output {INTERNAL} --off'
+    print(external_cmd)
     feh_cmd = "feh --bg-scale /home/$USER/Downloads/anime.jpg"
     subprocess.run(external_cmd,shell=True)
     subprocess.run(feh_cmd,shell=True)
 def internal():
-    internal_cmd = 'xrandr --output HDMI-1-1 --off'
+    internal_cmd = f'xrandr --output {EXTERNAL} --off'
     subprocess.run(internal_cmd,shell=True)
+
+def dynamic_monitor_name_fetch():
+    internal_like = "eDP"
+    external_like = "HDMI"
+    xrandr_output = subprocess.getoutput("xrandr")
+
+    output_list = xrandr_output.splitlines()
+
+    for each_line in output_list:
+        if internal_like in each_line:
+            # print("HIT")
+            global INTERNAL
+            INTERNAL = each_line.split(" ")[0]
+            # print(INTERNAL)
+
+        if external_like in each_line:
+            global EXTERNAL
+            global HDMI_CONNECT
+            EXTERNAL = each_line.split(" ")[0]
+            HDMI_CONNECT = f"{EXTERNAL} connected"
+
+
+    # print(INTERNAL)
+    # print(EXTERNAL)
 
 
 def main():
+    dynamic_monitor_name_fetch()
     try:
         parser = argparse.ArgumentParser()
         parser.add_argument(
@@ -64,6 +92,7 @@ def main():
         args = parser.parse_args()
 
         if args.left:
+            print("HIT")
             left()
         elif args.right:
             right()
